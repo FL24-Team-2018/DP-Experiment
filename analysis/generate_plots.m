53980f2af5c6f61134deeb55df66f3a8b1d51621
Thomas <thomas.wodzinski@gmail.com>
2019-03-18 10:37:15 +0000

    Update generate_plots.m

--- analysis/generate_plots.m
+++ analysis/generate_plots.m
@@ -14,11 +14,11 @@ subplot(nrows,1,2)
 % plot(sigma_est_um_array,corr_I_pixis_I_rec,'-xb');
 % xlabel('\sigma_{est}/um'), ylabel('corr\_I\_pixis\_I\_rec');
 plot(sigma_est_um_array,I_rec_profile_min,'-xb');
-xlabel('\sigma_{est}/um'), ylabel('minimum of reconstructed\newline profile I_{rec}');
+xlabel('\sigma_{est}/um'), ylabel('I_{rec,min}');
 
 subplot(nrows,1,3)
 plot(sigma_est_um_array,xi_um_hor,'-xb');
-xlabel('\sigma_{est}/um'), ylabel('coherence length \xi / um');
+xlabel('\sigma_{est}/um'), ylabel('\xi / um');
 
 subplot(nrows,1,4)
 plot(sigma_est_um_array,xi_um_hor/sigma_B_um,'-xb');
@@ -26,15 +26,15 @@ xlabel('\sigma_{est}/um'), ylabel('\xi/\sigma_B');
 
 subplot(nrows,1,5)
 plot(sigma_est_um_array,zeta,'-xb');
-xlabel('\sigma_{est}/um'), ylabel('norm. degree of tr. coherence \zeta / um');
+xlabel('\sigma_{est}/um'), ylabel('\zeta');
 
 subplot(nrows,1,6)
 plot(sigma_est_um_array,D,'-xb');
-xlabel('\sigma_{est}/um'), ylabel('distance between histograms of I_{pc,det} vs. I_{pc,rec}');
+xlabel('\sigma_{est}/um'), ylabel('histogram dist.\newlineI_{pc,det} vs. I_{pc,rec}');
 
 subplot(nrows,1,7)
 plot(sigma_est_um_array,corr_I_pixis_I_pc_rec,'-xb');
-xlabel('\sigma_{est}/um'), ylabel('correlation between histograms of I_{pc,det} vs. I_{pc,rec}');
+xlabel('\sigma_{est}/um'), ylabel('histogramm corr.\newline I_{pc,det} vs. I_{pc,rec}');
 
 
 
@@ -72,12 +72,12 @@ xi_um_hor(i)
 A_bp = ifourier2D(sqrt(I_pixis));  % amplitude
 I_bp = abs(A_bp).^2;  % intensity
 
-figure('rend', 'painters','pos', [1010 510 500 500]);
+figure('rend', 'painters','pos', [1010 510 1000 1000]);
 imagesc(X2_axis*R_1,Y2_axis*R_1,log10(I_bp));
 colormap(gca,jet);
 title('log10(I_{ph} = FT(I_{pixis}))\newline(backpropagated intensity at pinhole plate)');
 colorbar; 
-axis equal;
+axis equal tight;
 xlabel('x/um'), ylabel('y/um');
 
 saveas(gcf,fullfile(cd,folder,'intensity_at_pinholes'),'png');
@@ -85,9 +85,9 @@ saveas(gcf,fullfile(cd,folder,'intensity_at_pinholes'),'epsc');
 
 
 %%
-figure('rend', 'painters','pos', [1010 10 500 1500]);
+figure('rend', 'painters','pos', [1010 10 700 2100]);
 
-subplot(4,1,1);
+subplot(3,1,1);
 imagesc(X1_axis*R_1,Y1_axis*R_1,F_gamma_rec(:,:,i)); colormap(gca,jet); title(strcat('recovered F(\gamma) at CCD,\newline \sigma=',num2str(sigma_est_um_array(i)),'um',', iter=',num2str(iter),', \lambda = ',num2str(wavelength_nm),'nm', ', d=',num2str(d_um),'um'));
 colorbar;
 xlabel('x/um'), ylabel('y/um');
@@ -95,14 +95,14 @@ axis equal tight;
 xlim([-100 100]);
 ylim([-100 100]);
 
-subplot(4,1,2);
+subplot(3,1,2);
 imagesc(X2_axis*R_1,Y2_axis*R_1,abs_gamma(:,:,i)); colormap(gca,jet); title(strcat('recovered abs(\gamma) at Pinholes,\newline xi=',num2str(xi_um_hor(i)),'um'));colorbar;
 xlabel('x/um'), ylabel('y/um');
 axis equal tight;
 
 
 % lineout at pinhole plate position
-subplot(4,1,3)
+subplot(3,1,3)
 x = Y2_axis*R_1;
 %y = abs_gamma(:,round(n/2+1),i);  % vertical lineout of abs(gamma)
 y = abs_gamma(round(n/2+1),:,i);  % horizontal lineout of abs(gamma)
@@ -116,20 +116,20 @@ hold off
 xlabel('x/um'), ylabel('abs(\gamma)_{rec}');
 title(strcat('recovered abs(\gamma) at Pinholes,\newline xi=',num2str(xi_um_hor(i)),'um'));
 
-% lineout at pinhole plate position
-subplot(4,1,4)
-x = Y2_axis*R_1;
-y = abs_gamma(:,round(n/2+1),i);  % vertical lineout of abs(gamma)
-%y = abs_gamma(round(n/2+1),:,i);  % horizontal lineout of abs(gamma)
-% missing fit!
-sigma = xi_um_ver(i);
-y_fit = exp(-(x.^2)/(2*sigma^2));
-plot(x,y);
-hold on
-plot(x,y_fit);
-hold off
-xlabel('y/um'), ylabel('abs(\gamma)_{rec}');
-title(strcat('recovered abs(\gamma) at Pinholes,\newline xi=',num2str(xi_um_ver(i)),'um'));
+% % lineout at pinhole plate position
+% subplot(4,1,4)
+% x = Y2_axis*R_1;
+% y = abs_gamma(:,round(n/2+1),i);  % vertical lineout of abs(gamma)
+% %y = abs_gamma(round(n/2+1),:,i);  % horizontal lineout of abs(gamma)
+% % missing fit!
+% sigma = xi_um_ver(i);
+% y_fit = exp(-(x.^2)/(2*sigma^2));
+% plot(x,y);
+% hold on
+% plot(x,y_fit);
+% hold off
+% xlabel('y/um'), ylabel('abs(\gamma)_{rec}');
+% title(strcat('recovered abs(\gamma) at Pinholes,\newline xi=',num2str(xi_um_ver(i)),'um'));
 
 saveas(gcf,fullfile(cd,folder,'fgamma_and_gama'),'png');
 saveas(gcf,fullfile(cd,folder,'fgamma_and_gama'),'epsc');
@@ -165,7 +165,7 @@ title(strcat('I_{rec} * F(\gamma)_{rec} from blind deconvolution (D=', num2str(D
 figure('rend', 'painters','pos', [1010 100 1500 1000]);
 
 subplot(2,3,1);
-imagesc(X1_axis*R_1,Y1_axis*R_1,I_pixis); colormap(gca,jet); title(strcat('partially coherent measurement\newline I_{pixis} at CCD', ', \lambda = ',num2str(wavelength_nm),'nm', ', d=',num2str(d_um),'um')); colorbar; axis equal;
+imagesc(X1_axis*R_1,Y1_axis*R_1,I_pixis); colormap(gca,jet); title(strcat('partially coherent measurement\newline I_{pc} at CCD', ', \lambda = ',num2str(wavelength_nm),'nm', ', d=',num2str(d_um),'um')); colorbar; axis equal;
 xlabel('x/um'), ylabel('y/um');
 axis equal tight;
 
@@ -183,7 +183,7 @@ xlabel('x/um'), ylabel('y/um');
 colormap(gca,jet); colorbar;
 
 subplot(2,3,4);
-imagesc(X1_axis*R_1,Y1_axis*R_1,F_gamma_rec(:,:,i)); colormap(gca,jet); title(strcat('recovered F(\gamma)_{rec} at CCD,\newline\sigma_{est,Gauss}=',num2str(sigma_est_um_array(i)),'um',', iter=',num2str(iter)));colorbar; 
+imagesc(X1_axis*R_1,Y1_axis*R_1,F_gamma_rec(:,:,i)); colormap(gca,jet); title(strcat('recovered F(\gamma)_{rec} at CCD,\newline\sigma_{rec,Gauss}=',num2str(sigma_F_gamma_rec_hor_um(i)),'um'));colorbar; 
 %axis equal;
 xlabel('x/um'), ylabel('y/um');
 axis equal tight;
@@ -191,6 +191,32 @@ xlim([-100 100]);
 ylim([-100 100]);
 
 
+saveas(gcf,fullfile(cd,folder,strcat('reconstructioncomparison_',num2str(sigma_est_um_array(i)),'um')),'epsc');
+saveas(gcf,fullfile(cd,folder,strcat('reconstructioncomparison_',num2str(sigma_est_um_array(i)),'um')),'png');
+
+%% overview plots
+
+figure('rend', 'painters','pos', [1010 100 2600 700]);
+
+subplot(1,3,1);
+imagesc(X1_axis*R_1,Y1_axis*R_1,I_pixis); colormap(gca,jet); title(strcat('partially coherent measurement I_{pixis} at CCD', ', \lambda = ',num2str(wavelength_nm),'nm', ', d=',num2str(d_um),'um')); colorbar; axis equal;
+xlabel('x/um'), ylabel('y/um');
+axis equal tight;
+
+subplot(1,3,2);
+imagesc(X1_axis*R_1,Y1_axis*R_1,I_rec(:,:,i)); colormap(gca,jet);
+title(strcat('recovered fully coherent intensity I_{rec} at CCD'));colorbar; axis equal;
+caxis([0 1]);
+xlabel('x/um'), ylabel('y/um');
+axis equal tight;
+
+subplot(1,3,3);
+imagesc(X1_axis*R_1,Y1_axis*R_1,F_gamma_rec(:,:,i)); colormap(gca,jet); title(strcat('recovered F(\gamma)_{rec} at CCD, \sigma_{rec,Gauss}=',num2str(round(sigma_F_gamma_rec_hor_um(i),1)),'um'));colorbar; 
+%axis equal;
+xlabel('x/um'), ylabel('y/um');
+axis equal tight;
+xlim([-100 100]);
+ylim([-100 100]);
 
 % subplot(2,3,5);
 % imagesc(X1_axis*R_1,Y1_axis*R_1,F_gamma_rec2(:,:,i)); title(strcat('recovered F(\gamma)_{rec,Gauss} as Gaussian with \sigma_{rec,Gauss}=',num2str(Sigma_rec2_um(i)),'um')); 
@@ -204,13 +230,29 @@ ylim([-100 100]);
 % xlabel('x/um'), ylabel('y/um');
 % colormap(gca,jet); colorbar;
 
-saveas(gcf,fullfile(cd,folder,strcat('reconstructioncomparison_',num2str(sigma_est_um_array(i)),'um')),'epsc');
-saveas(gcf,fullfile(cd,folder,strcat('reconstructioncomparison_',num2str(sigma_est_um_array(i)),'um')),'png');
-
+saveas(gcf,fullfile(cd,folder,strcat('partiallycoherentimageandFgamma_',num2str(sigma_est_um_array(i)),'um')),'epsc');
+saveas(gcf,fullfile(cd,folder,strcat('partiallycoherentimageandFgamma_',num2str(sigma_est_um_array(i)),'um')),'png');
 
 %% lineouts of partially coherent measurement and fully coherent reconstruction
+figure('rend', 'painters','pos', [100 100 1500 1100],'visible','on');
+deltay = 25;
+I_pixis_profile = norm_function(mean(I_pixis(idx_row-deltay:idx_row+deltay,:)));
+min(I_pixis_profile(round(n/2)-200:round(n/2)+200))
+plot(X1_axis*R_1,I_pixis_profile,'-b', 'LineWidth',3);
+hold on
+plot(X1_axis*R_1,I_rec_profile(:,:,i),'-r', 'LineWidth',1);
+hold off
+%legend('I_{pc,data}','I_{c,rec}','FontSize', 24);
+legend({'partially coherent measurement','fully coherent reconstruction'});
+title(strcat('\lambda = ',num2str(wavelength_nm),'nm', ', separation d=',num2str(d_um),'um, \sigma_B=',num2str(sigma_B_um),'um, ', '\newline coherence length \xi=',num2str(round(xi_um_hor(i),1)),'um, \xi/\sigma_B=' ,num2str(round(xi_um_hor(i)/sigma_B_um,2)),', \zeta=',num2str(round(zeta(i),2))));
+xlim([-6500,6500])
+xlabel('detector position / um')
+ylabel('Intensity / a.u.')
+saveas(gcf,fullfile(cd,folder,strcat('profiles_sigma_est_um_opt_',num2str(sigma_est_um_array(i)),'um')),'epsc');
+saveas(gcf,fullfile(cd,folder,strcat('profiles_sigma_est_um_opt_',num2str(sigma_est_um_array(i)),'um')),'png');
+
 for i=1:length(sigma_est_um_array)
-    figure('rend', 'painters','pos', [10 10 1500 1100],'visible','off');
+    figure('rend', 'painters','pos', [100 100 1500 1100],'visible','off');
     deltay = 25;
     I_pixis_profile = norm_function(mean(I_pixis(idx_row-deltay:idx_row+deltay,:)));
     min(I_pixis_profile(round(n/2)-200:round(n/2)+200))
@@ -220,7 +262,7 @@ for i=1:length(sigma_est_um_array)
     hold off
     %legend('I_{pc,data}','I_{c,rec}','FontSize', 24);
     legend({'partially coherent measurement','fully coherent reconstruction'});
-    title(strcat('\lambda = ',num2str(wavelength_nm),'nm', ', separation d=',num2str(d_um),'um, ', '\newlinecoherence length \xi=',num2str(round(xi_um_hor(i),1)),'um', ' \zeta=',num2str(round(zeta(i),2))));
+    title(strcat('\lambda = ',num2str(wavelength_nm),'nm', ', separation d=',num2str(d_um),'um, \sigma_B=',num2str(sigma_B_um),'um, ', '\newline coherence length \xi=',num2str(round(xi_um_hor(i),1)),'um, \xi/\sigma_B=' ,num2str(round(xi_um_hor(i)/sigma_B_um,2)),', \zeta=',num2str(round(zeta(i),2))));
     xlim([-6500,6500])
     xlabel('detector position / um')
     ylabel('Intensity / a.u.')
@@ -229,14 +271,15 @@ for i=1:length(sigma_est_um_array)
     if ~exist(folder, 'dir')
     mkdir(folder);
     end
-    saveas(gcf,fullfile(cd,folder,strcat('profiles_',num2str(sigma_est_um_array(i)),'um')),'epsc');
-    saveas(gcf,fullfile(cd,folder,strcat('profiles_',num2str(sigma_est_um_array(i)),'um')),'png');
+    saveas(gcf,fullfile(cd,folder,strcat('profiles_sigma_est_um_',num2str(sigma_est_um_array(i)),'um')),'epsc');
+    saveas(gcf,fullfile(cd,folder,strcat('profiles_sigma_est_um_',num2str(sigma_est_um_array(i)),'um')),'png');
 end
 
 %% pinhole image
 figure('rend', 'painters','pos', [510 510 1000 1000]);
 imagesc(pinholes_image_norm); colormap(gca,jet);
-
+saveas(gcf,fullfile(cd,folder,'pinhole_image'),'epsc');
+saveas(gcf,fullfile(cd,folder,'pinhole_image'),'png');
 
 end
 
